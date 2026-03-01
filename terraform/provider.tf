7536b37068cf1e8528640686ea827418534cf6c6
Jayani22 <mutcherlajayani@gmail.com>
2026-03-01 20:19:41 +0530

    Initial commit - Terraform + Ansible + Monitoring stack

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -4,13 +4,7 @@ terraform{
             source = "hashicorp/aws"
             version = "6.16.0"
         }
-    }
-    # s3 "backend" {
-    #     name = "tf-state-backend-jayani"
-    #     key = "terraform-ec2"
-    #     encrypt = true
-    # }
-    
+    } 
 }
 
 provider "aws" {
